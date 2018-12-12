038ca15b8e81e08c33f70b5a9e84c447b701059c
GongUi <zhanggoni@gmail.com>
2018-12-12 20:39:02 +0900

    Initial Commit

---
+++ BoostCamp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BoostCamp
+//
+//  Created by 장공의 on 12/12/2018.
+//  Copyright © 2018 zhanggoniui. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
